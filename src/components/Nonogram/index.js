8562cae01a703ff9959019d494fc2b17d80003a3
寫詩羊 <Habuche@gmail.com>
2020-09-25 01:10:12 +0800

    基本設定

---
+++ src/components/Nonogram/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './style.module.scss';
+
+export default function Nonogram() {
+  return (
+    <div className={style.Nonogram}>
+      nonogram
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.module.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
 
 ReactDOM.render(
   <React.StrictMode>
