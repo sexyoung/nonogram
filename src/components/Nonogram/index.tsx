65f0a409664f3a2b80ad5af02981934d2d92918e
寫詩羊 <Habuche@gmail.com>
2020-10-22 02:30:36 +0800

    把水平格子切出來

--- src/components/Nonogram/index.tsx
+++ src/components/Nonogram/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useResize } from 'hooks';
-import { BitData, NonogramPropsType } from './index.d';
+import * as d from './index.d';
 
 import style from './style.module.scss';
 
-type Props = NonogramPropsType;
+type Props = d.NonogramPropsType;
 
-const longestLen = (data: BitData[]) =>
+const longestLen = (data: d.BitData[]) =>
   Math.max(...data.map(arr => arr.length));
 
 const Nonogram: React.FunctionComponent<Props> = ({
@@ -33,11 +33,19 @@ const Nonogram: React.FunctionComponent<Props> = ({
       <div className={style.firstRow} style={{flex: hMaxLen}}>
         <div className={style.empty} style={col1Style}></div>
         <div className={style.horizontalInfo} style={col2Style}>
-
+          {hData.map(hBitList =>
+            <div className={style.hBitList}>
+              {[...Array(hMaxLen).keys()].map(bit =>
+                <div className={style.bit}></div>
+              )}
+            </div>
+          )}
         </div>
       </div>
       <div className={style.secondRow} style={{flex: vData.length}}>
-        <div className={style.verticalInfo} style={col1Style}></div>
+        <div className={style.verticalInfo} style={col1Style}>
+          這裡的資料可以跟水平資料一樣，所以應該建議一個元件
+        </div>
         <div className={style.matrix} style={col2Style}>
 
         </div>
